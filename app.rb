da2c07449de60f3249a0b8cbd4c7d7572df4735c
contessa <tessa.fallon@gmail.com>
2013-12-09 14:38:53 -0500

    corrected

--- app.rb
+++ app.rb
@@ -1,28 +1,14 @@
-<<<<<<< HEAD
 require 'sinatra/base'
 
 
 module StudentSite
   class App < Sinatra::Base
+    get '/hello-world'do
+    erb :hello
+	 end
     get '/artists' do
 	@artists = ["Matisse", "Man Ray", "Reuben"]
 	erb :artists
 	end
    end
  end
-=======
-require 'bundler'
-Bundler.require
-
-class TestApp < Sinatra::Application
-
-get '/hello-world' do
-	erb :hello
-end
-
-get '/artists' do
-@artists = ["Matisse", "Man Ray", "Reuben"]
-	erb :artists
-end
-end
->>>>>>> c57f239f9d3fb528817e0f46c35580953b3a8f6f
--- config.ru
+++ config.ru
@@ -1,7 +1,3 @@
 require './app'
-<<<<<<< HEAD
 run StudentSite::App.new
-=======
 
-run TestApp
->>>>>>> c57f239f9d3fb528817e0f46c35580953b3a8f6f
